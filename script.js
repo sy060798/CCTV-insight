b074254e65097b0c5e16198981eb92d050fd1c56
Gog12 <bejop0433@gmail.com>
2025-08-03 01:29:09 +0700

    Update script.js
    Signed-off-by: Gog12 <bejop0433@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function tampilkanKoleksi() {
-  document.getElementById("beranda").classList.add("hidden");
-  document.getElementById("koleksi").classList.remove("hidden");
-}
+<!-- Script -->
+  <script>
+    function tampilkanKoleksi() {
+      document.getElementById("beranda").classList.add("hidden");
+      document.getElementById("koleksi").classList.remove("hidden");
+    }
 
-function kembaliKeBeranda() {
-  document.getElementById("koleksi").classList.add("hidden");
-  document.getElementById("beranda").classList.remove("hidden");
-}
+    function kembaliKeBeranda() {
+      document.getElementById("koleksi").classList.add("hidden");
+      document.getElementById("beranda").classList.remove("hidden");
+    }
+  </script>
