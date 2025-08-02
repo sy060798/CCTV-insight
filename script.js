dc4ac832b3897ab20bf95ae9a00c9d58b38e32c2
Gog12 <bejop0433@gmail.com>
2025-08-03 01:25:08 +0700

    Update script.js
    Signed-off-by: Gog12 <bejop0433@gmail.com>

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-<script>
-  // Saat DOM sudah siap
-  document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("menu-beranda").addEventListener("click", function (e) {
-      e.preventDefault(); // mencegah reload halaman
-      document.getElementById("koleksi").classList.add("hidden");
-      document.getElementById("beranda").classList.remove("hidden");
-    });
+function tampilkanKoleksi() {
+  document.getElementById("beranda").classList.add("hidden");
+  document.getElementById("koleksi").classList.remove("hidden");
+}
 
-    document.getElementById("menu-koleksi").addEventListener("click", function (e) {
-      e.preventDefault();
-      document.getElementById("beranda").classList.add("hidden");
-      document.getElementById("koleksi").classList.remove("hidden");
-    });
-  });
-</script>
+function kembaliKeBeranda() {
+  document.getElementById("koleksi").classList.add("hidden");
+  document.getElementById("beranda").classList.remove("hidden");
+}
